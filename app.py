a414e822ecaab80525ff2d0d202e9f610b501082
Vladimir Kalbin <kalbin.wow@yandex.ru>
2026-02-18 09:10:51 +0500

    Fix next scene

--- app.py
+++ app.py
@@ -449,7 +449,7 @@ def to_next_scene(story_id: int):
 
         user_save = db.load_game_raw(user.id, story_id)
         scene = s.query(Scene).filter(Scene.id > user_save.scene_id
-                                      and Scene.chapter_id == user_save.chapter_id
+                                      & Scene.chapter_id == user_save.chapter_id
                                       ).order_by(Scene.id).first()
 
         if not scene:
